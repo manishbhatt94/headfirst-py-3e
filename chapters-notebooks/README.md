4c8ce6be473a25a27e9f6b94d9aadcb8f56884c4
Manish Bhatt <manishbhatt594@gmail.com>
2025-08-08 20:45:32 +0530

    Copy files for webapp

--- chapters-notebooks/README.md
+++ chapters-notebooks/README.md
@@ -6,4 +6,5 @@
 - 03 - List of Files
 - 04 - Formatted String Literals
 - 05 - Data Structure Choices
+- 06 - Web Development
 
---
+++ chapters-notebooks/webapp/swimclub.py
@@ -0,0 +1,89 @@
+from statistics import mean
+
+import hfpy_utils
+
+FOLDER = "../swimdata/"
+CHARTS = "charts/"
+
+
+def read_swim_data(filename):
+    """Return swim data from a file.
+
+    Given the name of a swimmer's file (in filename), extract all the required
+    data, then return it to the caller as a tuple.
+    """
+    swimmer, age, distance, stroke = filename.removesuffix(".txt").split("-")
+
+    with open(FOLDER + filename) as file:
+        lines = file.readlines()
+        times = lines[0].strip().split(",")
+
+    converts = []
+
+    for t in times:
+        # The minutes value might be missing, so guard against that
+        if ":" in t:
+            minutes, rest = t.split(":")
+            seconds, hundredths = rest.split(".")
+        else:
+            minutes = 0
+            seconds, hundredths = t.split(".")
+        converted_time = (
+            (int(minutes) * 60 * 100) + (int(seconds) * 100) + int(hundredths)
+        )
+        converts.append(converted_time)
+
+    average = mean(converts)
+
+    mins_secs, hundredths = f"{(average / 100):.2f}".split(".")
+    mins_secs = int(mins_secs)
+    hundredths = int(hundredths)
+    minutes = mins_secs // 60
+    seconds = mins_secs % 60
+
+    average = f"{minutes:02d}:{seconds:0>2}.{hundredths}"
+
+    # Returned as a tuple
+    return swimmer, age, distance, stroke, times, average, converts
+
+
+def produce_bar_chart(fn):
+    """Given the name of a swimmer's file, produce a HTML/SVG-based bar chart.
+
+    Save the chart to the CHARTS folder. Return the path to the bar chart file.
+    """
+    swimmer, age, distance, stroke, times, average, converts = read_swim_data(fn)
+    from_max = max(converts)
+    times.reverse()
+    converts.reverse()
+    title = f"{swimmer} (Under {age}) {distance} {stroke}"
+
+    header = f"""<!DOCTYPE html>
+<html lang="en-IN">
+    <head>
+        <title>{title}</title>
+    </head>
+    <body>
+        <h3>{title}</h3>"""
+
+    body = ""
+
+    for n, t in enumerate(times):
+        bar_width = hfpy_utils.convert2range(converts[n], 0, from_max, 0, 350)
+        body += f"""
+        <svg height="30" width="400">
+            <rect height="30" width="{bar_width}" style="fill:rgb(0,0,255);" />
+        </svg>{t}<br />"""
+
+    footer = f"""
+        <p>Average time: {average}</p>
+    </body>
+</html>
+"""
+    page = header + body + footer
+
+    save_to = f"{CHARTS}{fn.removesuffix('.txt')}.html"
+    with open(save_to, "w") as sf:
+        print(page, file=sf)
+
+    return save_to
