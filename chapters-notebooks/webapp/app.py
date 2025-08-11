4996c16c786de0a869186e52ee8da86523125548
Manish Bhatt <manishbhatt594@gmail.com>
2025-08-11 18:14:55 +0530

    Render jinja2 templates to show swimmers list

--- chapters-notebooks/webapp/app.py
+++ chapters-notebooks/webapp/app.py
@@ -1,28 +1,49 @@
 import os
 
 import swimclub
-from flask import Flask
+from flask import Flask, render_template, session
 
 app = Flask(__name__)
+app.secret_key = "You will never guess..."
+
+
+def populate_data():
+    if "swimmers" not in session:
+        swim_files = os.listdir(swimclub.FOLDER)
+        if ".DS_Store" in swim_files:
+            swim_files.remove(".DS_Store")
+        session["swimmers"] = {}
+        for file in swim_files:
+            name, *_ = swimclub.read_swim_data(file)
+            if name not in session["swimmers"]:
+                session["swimmers"][name] = []
+            session["swimmers"][name].append(file)
 
 
 @app.get("/")
 def index():
-    return "This is a placeholder for your webapp's opening page."
+    return render_template(
+        "index.html",
+        title="Welcome to the Swimclub system",
+    )
 
 
 @app.get("/swimmers")
 def display_swimmers():
-    swim_files = os.listdir(swimclub.FOLDER)
-    if ".DS_Store" in swim_files:
-        swim_files.remove(".DS_Store")
-    swimmers = {}
-    for file in swim_files:
-        name, *_ = swimclub.read_swim_data(file)
-        if name not in swimmers:
-            swimmers[name] = []
-        swimmers[name].append(file)
-    return str(sorted(swimmers))
+    populate_data()
+    return render_template(
+        "select.html",
+        title="Select a swimmer",
+        url="/showfiles",
+        select_id="swimmer",
+        data=sorted(session["swimmers"]),
+    )
+
+
+@app.get("/files/<swimmer>")
+def get_swimmers_files(swimmer):
+    populate_data()
+    return str(session["swimmers"][swimmer])
 
 
 if __name__ == "__main__":
