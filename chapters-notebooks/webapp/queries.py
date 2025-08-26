56fc39861b48b22d02f242185730e15686a106c9
Manish Bhatt <manishbhatt594@gmail.com>
2025-08-26 17:14:42 +0530

    Use MariaDB specific query placeholder character
    Change from SQLite specific "?" placeholder char to MariaDB specific "%s" char.

--- chapters-notebooks/webapp/queries.py
+++ chapters-notebooks/webapp/queries.py
@@ -6,7 +6,7 @@ SQL_SESSIONS = """
 SQL_SWIMMERS_BY_SESSION = """
     select distinct swimmers.name, swimmers.age  
     from times, swimmers 
-    where date(times.ts) = ? and     
+    where date(times.ts) = %s and     
     times.swimmer_id = swimmers.id 
     order by name
 """
@@ -17,17 +17,17 @@ SQL_SWIMMERS_EVENTS_BY_SESSION = """
     from swimmers, events, times
     where times.swimmer_id = swimmers.id and
     times.event_id = events.id and
-    (swimmers.name = ? and swimmers.age = ?) and
-    date(times.ts) = ?
+    (swimmers.name = %s and swimmers.age = %s) and
+    date(times.ts) = %s
 """
 
 
 SQL_CHART_DATA_BY_SWIMMER_EVENT_SESSION = """
     select times.time
     from swimmers, events, times
-    where (swimmers.name = ? and swimmers.age = ?) and
-    (events.distance = ? and events.stroke = ?) and 
+    where (swimmers.name = %s and swimmers.age = %s) and
+    (events.distance = %s and events.stroke = %s) and 
     swimmers.id = times.swimmer_id and
     events.id = times.event_id and
-    date(times.ts) = ?
+    date(times.ts) = %s
 """
